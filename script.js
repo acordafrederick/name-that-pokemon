e75ac56c9572d1460ba1d5ad6da5f10bdafb39cd
acordafrederick <sonjaire@gmail.com>
2019-07-21 17:05:30 -0500

    Populated timer to index.html. Created pokemon var for trivia questions.

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
+var pokemon = [
+    {
+        "name": "Vaporeon",
+        "sprite": "https://cdn.bulbagarden.net/upload/f/fd/134Vaporeon.png",
+        "answers": ["Vaporeon", "Jolteon", "Flareon"]
+    },
+    {
+        "name": "Jolteon",
+        "sprite": "https://cdn.bulbagarden.net/upload/b/bb/135Jolteon.png",
+        "answers": ["Vaporeon", "Jolteon", "Flareon"]
+    },
+    {
+        "name": "Flareon",
+        "sprite": "https://cdn.bulbagarden.net/upload/d/dd/136Flareon.png",
+        "answers": ["Vaporeon", "Jolteon", "Flareon"]
+    }
+]
+
+// function selectPokemon() {
+//     for (var i = 0; i <= pokemon.length; i++) {
+//         console.log(pokemon[i]["name"]);
+//     }
+// }
+// selectPokemon();
 
 
 function startTimer() {
-    var count = 30;
-    console.log(timer);
+    var count = 29;
+    var timerHtml = document.getElementById("timer");
     var countDown = setInterval(function() {
-        console.log(count);
+        // console.log(count);
+        timerHtml.innerHTML = count;
         count--;
         if (count === 0) {
             clearInterval(countDown);
