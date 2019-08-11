036d57667dde413039f78adba07a23ec2f8d6afe
acordafrederick <sonjaire@gmail.com>
2019-08-11 12:54:24 -0500

    Created function iterating thru questions, and functions counting correct and incorrect answers; Initial styling for the HTML.

--- script.js
+++ script.js
@@ -1,38 +1,108 @@
-var pokemon = [
+// array of questions user need to answer
+var questions = [
     {
-        "name": "Vaporeon",
-        "sprite": "https://cdn.bulbagarden.net/upload/f/fd/134Vaporeon.png",
-        "answers": ["Vaporeon", "Jolteon", "Flareon"]
+      "name": "Bulbasaur",
+      "sprite": "https://cdn.bulbagarden.net/upload/2/21/001Bulbasaur.png",
+      "choices": ["Caterpie", "Charmander", "Bulbasaur", "Squirtle"]
     },
     {
-        "name": "Jolteon",
-        "sprite": "https://cdn.bulbagarden.net/upload/b/bb/135Jolteon.png",
-        "answers": ["Vaporeon", "Jolteon", "Flareon"]
+      "name": "Chikorita",
+      "sprite": "https://cdn.bulbagarden.net/upload/b/bf/152Chikorita.png",
+      "choices": ["Chikorita", "Sentret", "Totodile", "Cyndaquil"]
     },
     {
-        "name": "Flareon",
-        "sprite": "https://cdn.bulbagarden.net/upload/d/dd/136Flareon.png",
-        "answers": ["Vaporeon", "Jolteon", "Flareon"]
+      "name": "Treecko",
+      "sprite": "https://cdn.bulbagarden.net/upload/2/2c/252Treecko.png",
+      "choices": ["Torchic", "Mudkip", "Poochyena", "Treecko"]
+    },
+    {
+      "name": "Turtwig",
+      "sprite": "https://cdn.bulbagarden.net/upload/5/5c/387Turtwig.png",
+      "choices": ["Turtwig", "Chimchar", "Piplup", "Starly"]
+    },
+    {
+      "name": "Snivy",
+      "sprite": "https://cdn.bulbagarden.net/upload/7/75/495Snivy.png",
+      "choices": ["Oshawott", "Tepig", "Patrat", "Snivy"]
+    },
+    {
+      "name": "Chespin",
+      "sprite": "https://cdn.bulbagarden.net/upload/c/ca/650Chespin.png",
+      "choices": ["Fennekin", "Bunnelby", "Chespin", "Froakie"]
+    },
+    {
+      "name": "Rowlet",
+      "sprite": "https://cdn.bulbagarden.net/upload/7/74/722Rowlet.png",
+      "choices": ["Pikipek", "Rowlet", "Popplio", "Litten"]
     }
-]
+];
 
-// function selectPokemon() {
-//     for (var i = 0; i <= pokemon.length; i++) {
-//         console.log(pokemon[i]["name"]);
-//     }
-// }
-// selectPokemon();
-
-
-function startTimer() {
-    var count = 29;
-    var timerHtml = document.getElementById("timer");
-    var countDown = setInterval(function() {
-        // console.log(count);
-        timerHtml.innerHTML = count;
-        count--;
-        if (count === 0) {
-            clearInterval(countDown);
-        }
-    }, 1000);
+var currentQuestion = 0;
+// counter for incorrect asnwers
+var incorrect = 0;
+// counter for correct answers
+var correct = 0;
+// 30 seconds countdown timer for each iteration of question
+var seconds = 30;
+// show user answer is correct
+function check() {
+    console.log("Correct");
+    next()
 }
+// show user the correct answer if answer is wrong
+function wrong() {
+    console.log("Incorrect");
+    next();
+}
+// play button to start game
+
+  // go to question 1
+function play() {
+    //   console.log(questions[currentQuestion]["sprite"]);
+    document.getElementById("sprite").setAttribute("src", questions[currentQuestion]["sprite"]);
+    document.getElementById("sprite").setAttribute("alt", questions[currentQuestion]["name"]);
+    //   console.log(questions[currentQuestion]["choices"]);
+    for (var i = 0; i < questions[currentQuestion]["choices"].length; i++) {
+        var parent = document.getElementById("options");
+        var child = document.createElement("button");
+        child.setAttribute("value", questions[currentQuestion]["choices"][i]);
+        child.setAttribute("type", "button");
+        child.append(questions[currentQuestion]["choices"][i]);
+        parent.append(child);
+        if (child.getAttribute("value") === questions[currentQuestion]["name"]) {
+            child.setAttribute("onclick", "check()");
+        } else {
+            child.setAttribute("onclick", "wrong()");
+        }
+    }
+}
+
+function next() {
+    if (currentQuestion <= questions.length) {
+        currentQuestion++;
+        play();
+    }
+}
+
+
+
+    // if time is still going and user's answer is correct
+      // timer stops
+      // correct answer counter increments
+      // tell user answer is correct
+      // go to next question
+    // if time is still going and user's answer is incorrect
+      // timer stops
+      // incorrect answer counter increments
+      // tell user answer is wrong
+      // show the correct answer
+      // go to next question
+    // if user does not have anser and time is up
+      // say time's up
+      // incorrect answer counter increments
+      // show the correct answer
+      // go to next question
+  // if all questions have been iterated and answered
+    // show tally of incorrect and correct answers
+    // show option to play again
+//   if user hit play again, repeat above steps
