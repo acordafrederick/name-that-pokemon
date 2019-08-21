9901e5fb0ee9da5c548a5e8b204a224f56f34377
Frederick Acorda <sonjaire@gmail.com>
2019-08-21 07:07:42 -0500

    Updated questions. Populated first question.

--- script.js
+++ script.js
@@ -1,127 +1,61 @@
 $("#play").hide();
 
 $(document).ready(function() {
-  
   var questions = [
     {
-      name: "Bulbasaur",
-      sprite: "https://cdn.bulbagarden.net/upload/2/21/001Bulbasaur.png",
-      choices: ["Finneon", "Lumineon", "Empoleon", "Bulbasaur"]
+      name: "Blacephalon",
+      sprite: "https://cdn.bulbagarden.net/upload/a/a5/806Blacephalon.png",
+      choices: ["Blacephalon", "Reshiram", "Chandelure", "Heatmor"]
     },
     {
-      name: "Cyndaquil",
-      sprite: "https://cdn.bulbagarden.net/upload/9/9b/155Cyndaquil.png",
-      choices: ["Meganium", "Virizion", "Cyndaquil", "Ferrothorn"]
+      name: "Buzzwole",
+      sprite: "https://cdn.bulbagarden.net/upload/f/fa/794Buzzwole.png",
+      choices: ["Scizor", "Buzzwole", "	Shelmet", "Scyther"]
     },
     {
-      name: "Mudkip",
-      sprite: "https://cdn.bulbagarden.net/upload/6/60/258Mudkip.png",
-      choices: ["Mudkip", "Dewgong", "Glaceon", "Umbreon"]
+      name: "Celesteela",
+      sprite: "https://cdn.bulbagarden.net/upload/8/89/797Celesteela.png",
+      choices: ["Bronzong", "Skarmory", "	Metagross", "Celesteela"]
     },
     {
-      name: "Turtwig",
-      sprite: "https://cdn.bulbagarden.net/upload/5/5c/387Turtwig.png",
-      choices: ["Turtwig", "Umbreo", "Vaporeon", "Starly"]
+      name: "Guzzlord",
+      sprite: "https://cdn.bulbagarden.net/upload/4/47/799Guzzlord.png",
+      choices: ["Darkrai", "Yveltal", "Zoroark", "Guzzlord"]
     },
     {
-      name: "Tepig",
-      sprite: "https://cdn.bulbagarden.net/upload/5/5b/498Tepig.png",
-      choices: ["Oshawott", "Tepig", "Patrat", "Tepig"]
+      name: "Kartana",
+      sprite: "https://cdn.bulbagarden.net/upload/f/fe/798Kartana.png",
+      choices: ["Cacnea", "Sceptile", "Kartana", "Fomantis"]
     },
     {
-      name: "Froakie",
-      sprite: "https://cdn.bulbagarden.net/upload/1/18/656Froakie.png",
-      choices: ["Fennekin", "Froakie", "Chespin", "Froakie"]
+      name: "Naganadel",
+      sprite: "https://cdn.bulbagarden.net/upload/d/de/804Naganadel.png",
+      choices: ["Rayquaza", "Druddigon", "Naganadel", "Haxorus"]
     },
     {
-      name: "Rowlet",
-      sprite: "https://cdn.bulbagarden.net/upload/7/74/722Rowlet.png",
-      choices: ["Pikipek", "Rowlet", "Popplio", "Rowlet"]
+      name: "Nihilego",
+      sprite: "https://cdn.bulbagarden.net/upload/2/2c/793Nihilego.png",
+      choices: ["Nihilego", "Trubbish", "Grimer", "Amoonguss"]
     },
     {
-      name: "Scorbunny",
-      sprite: "https://cdn.bulbagarden.net/upload/c/cc/Scorbunny.png",
-      choices: ["Pikipek", "Rowlet", "Popplio", "Scorbunny"]
+      name: "Xurkitree",
+      sprite: "https://cdn.bulbagarden.net/upload/d/d2/796Xurkitree.png",
+      choices: ["lectivire", "Zeraora", "Xurkitree", "Eelektross"]
     },
     {
-      name: "Squirtle",
-      sprite: "https://cdn.bulbagarden.net/upload/3/39/007Squirtle.pngg",
-      choices: ["Pikipek", "Rowlet", "Squirtle", "Scorbunny"]
+      name: "Stakatak",
+      sprite: "https://cdn.bulbagarden.net/upload/2/27/805Stakataka.png",
+      choices: ["Magnezone", "Shieldon", "Magearna", "Stakatak"]
     },
     {
-      name: "Chikorita",
-      sprite: "https://cdn.bulbagarden.net/upload/b/bf/152Chikorita.png",
-      choices: ["Pikipek", "Chikorita", "Popplio", "Scorbunny"]
+      name: "Poipole",
+      sprite: "https://cdn.bulbagarden.net/upload/e/e5/803Poipole.png",
+      choices: ["Garbodor", "Poipole", "Ariados", "Toxapex"]
     },
     {
-      name: "Torchic",
-      sprite: "https://cdn.bulbagarden.net/upload/9/91/255Torchic.png",
-      choices: ["Torchic", "Rowlet", "Popplio", "Scorbunny"]
-    },
-    {
-      name: "Piplup",
-      sprite: "https://cdn.bulbagarden.net/upload/b/b1/393Piplup.png",
-      choices: ["Pikipek", "Rowlet", "Popplio", "Piplup"]
-    },
-    {
-      name: "Snivy",
-      sprite: "https://cdn.bulbagarden.net/upload/7/75/495Snivy.png",
-      choices: ["Pikipek", "Rowlet", "Popplio", "Snivy"]
-    },
-    {
-      name: "Fennekin",
-      sprite: "https://cdn.bulbagarden.net/upload/3/3d/653Fennekin.png",
-      choices: ["Pikipek", "Fennekin", "Popplio", "Scorbunny"]
-    },
-    {
-      name: "Popplio",
-      sprite: "https://cdn.bulbagarden.net/upload/d/d8/728Popplio.png",
-      choices: ["Pikipek", "Popplio", "Popplio", "Scorbunny"]
-    },
-    {
-      name: "Grookeyo",
-      sprite: "https://cdn.bulbagarden.net/upload/7/7a/Grookey.png",
-      choices: ["Grookey", "Popplio", "Popplio", "Scorbunny"]
-    },
-    {
-      name: "Charmander",
-      sprite: "https://cdn.bulbagarden.net/upload/7/73/004Charmander.png",
-      choices: ["Pikipek", "Popplio", "Popplio", "Charmander"]
-    },
-    {
-      name: "Totodile",
-      sprite: "https://cdn.bulbagarden.net/upload/d/df/158Totodile.png",
-      choices: ["Pikipek", "Popplio", "Totodile", "Scorbunny"]
-    },
-    {
-      name: "Treecko",
-      sprite: "https://cdn.bulbagarden.net/upload/2/2c/252Treecko.png",
-      choices: ["Treecko", "Popplio", "Popplio", "Scorbunny"]
-    },
-    {
-      name: "Chimchar",
-      sprite: "https://cdn.bulbagarden.net/upload/7/76/390Chimchar.png",
-      choices: ["Pikipek", "Popplio", "Popplio", "Chimchar"]
-    },
-    {
-      name: "Oshawott",
-      sprite: "https://cdn.bulbagarden.net/upload/3/3b/501Oshawott.png",
-      choices: ["Oshawott", "Popplio", "Popplio", "Scorbunny"]
-    },
-    {
-      name: "Chespin",
-      sprite: "https://cdn.bulbagarden.net/upload/c/ca/650Chespin.png",
-      choices: ["Chespin", "Popplio", "Popplio", "Scorbunny"]
-    },
-    {
-      name: "Litten",
-      sprite: "https://cdn.bulbagarden.net/upload/0/0e/725Litten.png",
-      choices: ["Litten", "Popplio", "Popplio", "Scorbunny"]
-    },
-    {
-      name: "Sobble",
-      sprite: "https://cdn.bulbagarden.net/upload/0/0c/Sobble.pngg",
-      choices: ["Pikipek", "Popplio", "Sobble", "Scorbunny"]
+      name: "Pheromosa",
+      sprite: "https://cdn.bulbagarden.net/upload/c/c7/795Pheromosa.png",
+      choices: ["Masquerain", "Pheromosa", "Ledyba", "Karrablast"]
     }
   ];
 
@@ -129,26 +63,24 @@ $(document).ready(function() {
   var incorrectCount = 0;
   var currentQuestion = 0;
   var seconds = 30;
-  
+
+  setTimeout(function() {
+    $("#play").show();
+  }, 5000);
+
+  $("#play").click(function() {
+    $(this).hide();
+    play();
+  });
+
   function play() {
     $("img").attr({
       src: questions[currentQuestion]["sprite"],
       alt: questions[currentQuestion]["name"]
     });
     for (var i = 0; i < questions[currentQuestion]["choices"].length; i++) {
-      $("#options").append(
-        "<button>" + questions[currentQuestion]["choices"][i] + "</button>"
-      );
+      $("#options").append("<button value='" + questions[currentQuestion]["choices"][i] +
+                           "' type='button'>" + questions[currentQuestion]["choices"][i] + "</button>");
     }
   }
-  
-  setTimeout(function() {
-    $("#play").show();
-  }, 5000);
-  
-  $("#play").click(function () { // call to play function
-    $(this).hide();
-    play();
-  });
-  
 }); // end of document.ready function
